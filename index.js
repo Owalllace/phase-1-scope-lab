95d14ec98f8bbe197734d61c6a99a2b1272b827d
OliviaWallace <oliviawallacepdx@gmail.com>
2024-02-08 19:52:22 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"; //considered a global scope variable
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = 'Olivia';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Bob';
+    return leastFavoriteCustomer;
+}
